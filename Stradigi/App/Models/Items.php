eb435b50b8f821eefe336fc19820137365d35e76
Natalia <nzader@crucialinteractive.com>
2017-03-26 21:49:03 -0400

    Update Items.php

--- Stradigi/App/Models/Items.php
+++ Stradigi/App/Models/Items.php
@@ -10,10 +10,8 @@ class Items extends \Core\Model
     public static function getAllItems()
     {
         $path = __DIR__  . '\panelPageContent.json';
-		$items = file_get_contents($path);
-		
-		$json = json_decode($items, true);
-		return $json;
-        
+ 	$items = file_get_contents($path);
+	$json = json_decode($items, true);
+	return $json;      
     }
 }
